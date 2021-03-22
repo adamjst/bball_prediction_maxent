524d8278c8d4e023e1f4ee9c68b471d2eebd17dc
adamjst <30847837+adamjst@users.noreply.github.com>
2021-03-22 16:01:45 -0400

    Add files via upload

--- step7_maxent_applied.py
+++ step7_maxent_applied.py
@@ -1,9 +1,16 @@
 import MaxEnt
 from pathlib import Path
+import pandas as pd
 import numpy as np
 
 # I don't totally understand the constraint syntax, but this works for solving the 2D case: reconstructing a 2D matrix with two 1D marginals
 constraint = ((1,),(0,) )
+#print(constraint)
+
+
+# create empty data frames for appending
+div_maxent_list = []
+div_marginals_list = []
 
 def step7_maxent(division, year_start, years_back):
     #Create loop counter for years
@@ -14,29 +21,45 @@ def step7_maxent(division, year_start, years_back):
     for yr in yrs:
         #path for csv based on division and year arguments
         step6_array = Path('step7_divisions', '{}_{}.csv'.format(division, yr), header=True)
-        #write csv path (not used yet)
-        step7_maxent = Path('{}_{}_step7.csv'.format(division, yr), header=True)
-
+        # write csv path for maxent
+        step7_maxent = Path('{}_step7_maxent.csv'.format(division, header=True))
+        # write csv path for marginals
+        step7_marginals = Path('{}_step7_marginals.csv'.format(division, header=True))
         #load csv and remove header team-names
         data = np.genfromtxt(step6_array, delimiter=',', skip_header=1)
         #convert csv to array and remove rowname team-names
         array = np.array(data, dtype=float)
+        #print(array)
         array = array[:, 1:]
 
-        #run model and print results
+        #run model
         m5 = MaxEnt.model(array, constraint)
         m5._GIS()
-        #
-        print(m5.pdf_maxent)
-        print(m5.marginals)
-        # >>> m5.pdf_maxent
-        # array([[0.24729975, 0.12499633, 0.12499633],
-        #        [0.12499633, 0.06317873, 0.06317873],
-        #        [0.12499633, 0.06317873, 0.06317873]])
-        # m5.marginals
-        # >>> m5.marginals
-        # {(): 1.0, (0,): array([0.5 , 0.25, 0.25]), (1,): array([0.5 , 0.25, 0.25])}
 
+        #Convert to df and print results
+        div_maxent_df = pd.DataFrame(m5.pdf_maxent)
+        print(div_maxent_df)
+        div_marginals_df = pd.DataFrame(m5.marginals)
+        print(div_marginals_df)
+
+        #add season year
+        div_maxent_df['yr'] = yr
+        div_marginals_df['yr'] = yr
+
+        #append to list of all seasons
+        div_maxent_list.append(div_maxent_df)
+        div_marginals_list.append(div_marginals_df)
+
+    #concatenate all lists together
+    div_maxent_df = pd.concat(div_maxent_list, sort=False)
+    div_marginals_df = pd.concat(div_marginals_list, sort=False)
+
+    #write to csv
+    div_maxent_df.to_csv((step7_maxent), header=True, line_terminator='\n')
+    div_marginals_df.to_csv((step7_marginals), header=True, line_terminator='\n')
+
+
+#There have been various realignments over the years. Data goes back to 1995
 step7_maxent("atlantic", 2017, 22)
 step7_maxent("central", 2017, 22)
 step7_maxent("midwest", 2004, 9)
@@ -45,3 +68,4 @@ step7_maxent("northwest", 2017, 12)
 step7_maxent("pacific", 2017, 22)
 step7_maxent("southeast", 2017, 12)
 step7_maxent("southwest", 2017, 8)
+
